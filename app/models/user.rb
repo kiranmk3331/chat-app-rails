6929024d0860be5a0e1dcfb8418c77a9d976f0cb
kiranmk3331 <kiranmk3331@gmail.com>
2022-01-29 15:21:12 +0530

    render message from database

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   validates :username, presence: true, length: { minimum: 3, maximum: 20 }, uniqueness: true
-  has_secure_password
   has_many :messages
+  has_secure_password
 end
